ff7ef337551446ed981cf723b11163bcbe4b3328
adam5096 <adamchou5096@gmail.com>
2025-03-20 11:04:24 +0800

    update:adjust function include export syntax

--- education_count.js
+++ education_count.js
@@ -1,5 +1,8 @@
+// ES Modules 版本 (使用 import/export)
+// 檔名: education_module.js
+
 // 先向伺服器取回資料
-const getInterviewers = async function () {
+export const getInterviewers = async function () {
   try {
     const result = await fetch(
       "https://raw.githubusercontent.com/hexschool/2021-ui-frontend-job/master/frontend_data.json"
@@ -13,7 +16,7 @@ const getInterviewers = async function () {
 };
 
 // 統計教育程度種類及數量的函數 (使用函數表達式)
-const countEducation = (data) => {
+export const countEducation = (data) => {
   // 準備一個物件來存放不同教育程度的計數
   const educationCount = {};
 
@@ -23,11 +26,7 @@ const countEducation = (data) => {
     const education = person.education;
 
     // 如果這個教育程度已經在計數中，就加1；否則設為1
-    if (educationCount[education]) {
-      educationCount[education] += 1;
-    } else {
-      educationCount[education] = 1;
-    }
+    educationCount[education] = (educationCount[education] || 0) + 1;
   });
 
   // 將結果轉換為陣列並依數量排序（由高到低）
@@ -48,7 +47,17 @@ const countEducation = (data) => {
   return educationCount;
 };
 
-// 直接在 getInterviewers 後呼叫 countEducation
-getInterviewers().then((data) => {
-  countEducation(data);
-});
+// 統合功能的函數 - 可選，如果需要一個整合的函數
+export const analyzeEducation = async () => {
+  const data = await getInterviewers();
+  return countEducation(data);
+};
+
+// 這個文件作為模組時，不會自動執行分析
+// 如果希望直接執行，可以取消下面的註解
+
+// if (require.main === module) {
+//   analyzeEducation();
+// }
+
+analyzeEducation();
